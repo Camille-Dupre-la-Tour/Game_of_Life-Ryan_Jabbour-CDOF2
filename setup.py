16fa99924bf5c293ffafee033808f6012b9803fc
Ryan Jabbour <jabbourryan2@gmail.com>
2025-01-13 15:44:03 +0100

    Project Version 1.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+ name='{Game_of_Life-Ryan_Jabbour-CDOF2}',
+ version='1.0',
+ author='{Ryan_Jabbour}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
